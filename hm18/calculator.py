29c6bab12562e92b8c87f389201dbe2ec8ee49b9
Alesia Kapashilova <alesia.andreevna94@gmail.com>
2022-04-10 23:13:44 +0300

    update hm18

--- hm18/calculator.py
+++ hm18/calculator.py
@@ -2,6 +2,11 @@ class Calculator:
     def __init__(self):
         pass
 
+    @staticmethod
+    def is_valid(a, b):
+        assert all(isinstance(number, (float, int)) for number in (a, b)), \
+            "Вы ввели первое или второе число с другимм типом данных."
+
     def sum(self, a, b):
         return a + b
 
--- hm18/test_calculator.py
+++ hm18/test_calculator.py
@@ -10,6 +10,9 @@ class TestCalculator(unittest.TestCase):
     def test_sum_positiv(self):
         self.assertEqual(self.calculator.sum(1, 5), 6)
 
+    def test_type_float(self):
+        self.assertEqual(self.calculator.sum(1.55, 18), 19.55)
+
     def test_dif_positiv(self):
         self.assertEqual(self.calculator.dif(8, 5), 3)
 
@@ -27,6 +30,12 @@ class TestCalculator(unittest.TestCase):
 
     def test_mul_negativ(self):
         self.assertNotEqual(self.calculator.mul(10, 5), 17)
+
+    def test_type_error_one(self):
+        self.assertRaises(TypeError, self.calculator.mul(10, 'a'), 17)
+
+    def test_type_error_two(self):
+        self.assertRaises(TypeError, self.calculator.mul("b", 8), 10)
 
     def test_div_error(self):
         self.assertRaises(ZeroDivisionError, self.calculator.div, 10, 0)
