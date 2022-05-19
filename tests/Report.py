963ed7278bbbfa0f188c91030d64dec289a68ad9
Alesia Kapashilova <96441255+AlesiaKapashilova@users.noreply.github.com>
2022-05-19 22:23:28 +0300

    Update and rename test_fixture1.py to Report.py

---
+++ tests/Report.py
@@ -0,0 +1,4 @@
+def report_step(step_title):
+    with allure.step(step_title):
+        pass
+    
--- tests/test_fixture1.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.fixture(scope="class")
-
-def get_user():
-    print("*** fixture ***")
-
-def test(get_user):
-    pass
