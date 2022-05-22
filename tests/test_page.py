040ca0e39f82de7129ee6284ee1c296e8286bb06
Alesia Kapashilova <96441255+AlesiaKapashilova@users.noreply.github.com>
2022-05-22 21:25:41 +0300

    Update test_page.py

--- tests/test_page.py
+++ tests/test_page.py
@@ -11,7 +11,7 @@ class TestMyStore(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:        
-        cls.browser.binary_location = r"/opt/google/chrome"
+        cls.browser.binary_location = r"/etc/alternative/google-chrome"
         cls.browser = webdriver.Chrome(
             "/home/alesya/les/homework/tests/chromedriver")
         cls.browser.maximize_window()
